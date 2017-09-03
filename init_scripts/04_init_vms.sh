a762137ea098c465d643d5b51c171fc8ea36ac5e
wrmay <rmay@pivotal.io>
2017-09-03 16:12:26 -0400

    init_vms should set GEMFIRE, not GEMFIRE_HOME

--- init_scripts/04_init_vms.sh
+++ init_scripts/04_init_vms.sh
@@ -73,8 +73,8 @@ else
 fi
 
 echo "export JAVA_HOME=/usr/java/jdk1.8.0_144" >> /home/$GEMFIRE_USER/.bashrc
-echo "export GEMFIRE_HOME=/usr/local/gemfire" >> /home/$GEMFIRE_USER/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$GEMFIRE_HOME/bin:$PATH" >> /home/$GEMFIRE_USER/.bashrc
+echo "export GEMFIRE=/usr/local/gemfire" >> /home/$GEMFIRE_USER/.bashrc
+echo "export PATH='$JAVA_HOME/bin:$GEMFIRE/bin:$PATH'" >> /home/$GEMFIRE_USER/.bashrc
 
 
 echo "Finished executing 04_init_vms.sh"
