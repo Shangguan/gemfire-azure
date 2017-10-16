6e39c0bb89ebfcbf10e256174cbd8faec53cea0a
wrmay <rmay@pivotal.io>
2017-10-16 18:40:49 -0400

    and another fix to 05_install_maven

--- init_scripts/05_install_maven.py
+++ init_scripts/05_install_maven.py
@@ -58,14 +58,13 @@ if __name__ == '__main__':
     if uid is None or gid is None:
      sys.exit('could not find user: ' + gemuser)
 
-    here = os.path.dirname(os.path.abspath(sys.argv[0]))
     userDir = '/home/{0}'.format(gemuser)
     m2Dir = os.path.join(userDir,'.m2')
     if not os.path.exists(m2Dir):
       os.mkdir(m2Dir)
       os.chown(m2Dir,uid,gid)
 
-    shutil.copy(os.path.join(here,'settings.xml'), m2Dir)
+    shutil.copy('settings.xml', m2Dir)
 
     os.chown(os.path.join(m2Dir,'settings.xml'), uid, gid)
     print 'configured maven settings for ' + gemuser
