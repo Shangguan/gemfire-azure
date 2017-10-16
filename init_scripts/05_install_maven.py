8ae6ada39b4044c06c63d42cbc6c5bb5bdfd47e1
wrmay <rmay@pivotal.io>
2017-10-16 18:20:18 -0400

    more fixes to install_maven

--- init_scripts/05_install_maven.py
+++ init_scripts/05_install_maven.py
@@ -47,25 +47,25 @@ if __name__ == '__main__':
 
         print 'installed {1}'.format( name)
 
-      uid = None
-      gid = None
-      for user in pwd.getpwall():
-         if user[0] == gemuser:
-            uid = user[2]
-            gid = user[3]
-            break
+    uid = None
+    gid = None
+    for user in pwd.getpwall():
+     if user[0] == gemuser:
+        uid = user[2]
+        gid = user[3]
+        break
 
-      if uid is None or gid is None:
-         sys.exit('could not find user: ' + gemuser)
+    if uid is None or gid is None:
+     sys.exit('could not find user: ' + gemuser)
 
-      here = os.path.dirname(os.path.abspath(sys.argv[0]))
-      userDir = '/home/{0}'.format(gemuser)
-      m2Dir = os.path.join(userDir,'.m2')
-      if not os.path.exists(m2Dir):
-          os.mkdir(m2Dir)
-          os.chown(m2Dir,uid,gid)
+    here = os.path.dirname(os.path.abspath(sys.argv[0]))
+    userDir = '/home/{0}'.format(gemuser)
+    m2Dir = os.path.join(userDir,'.m2')
+    if not os.path.exists(m2Dir):
+      os.mkdir(m2Dir)
+      os.chown(m2Dir,uid,gid)
 
-      shutil.copy(os.path.join(here,'settings.xml'), m2Dir)
+    shutil.copy(os.path.join(here,'settings.xml'), m2Dir)
 
-      os.chown(os.path.join(m2Dir,'settings.xml'), uid, gid)
-      print 'configured maven settings for ' + gemuser
+    os.chown(os.path.join(m2Dir,'settings.xml'), uid, gid)
+    print 'configured maven settings for ' + gemuser
