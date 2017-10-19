a4d911ff726a53dc8485f678940bdae75f412a50
rmcphail <rmcphail@pivotal.io>
2017-10-18 21:33:16 -0400

    Update 20_set_motd.sh

--- init_scripts/20_set_motd.sh
+++ init_scripts/20_set_motd.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
-# This script creates the motd
+# This script:
+#   creates directory /opt/pivotal/gemfire
+#   copies file gemfire_validation_doc.md to directory /opt/pivotal/gemfire/
+#   creates the motd
 
 set -e
 
@@ -8,6 +11,8 @@ echo "### Starting script $0 .........."
 
 INSTALL_DIR="/opt/pivotal/gemfire"
 [[ ! -d ${INSTALL_DIR} ]] && { mkdir -p $INSTALL_DIR; chown gfadmin:gfadmin ${INSTALL_DIR}; }
+
+mv /home/gfadmin/gemfire-azure/gemfire_validation_doc.md /opt/pivotal/gemfire/gemfire_validation_doc.md
 
 gemfire_version=$(ls  -r /usr/local/ | grep pivotal-gemfire)
 
@@ -17,8 +22,14 @@ Welcome to your Pivotal Gemfire in-memory Data Grid on Azure!
 *********************************************************************************
 Gemfire Version:  $gemfire_version
 
-Display Gemfire Host names and IP addresses 
+Display DNS names and IP addresses 
    - From the Linux command prompt:  cat /etc/hosts
+
+Run some validation steps to get started
+   - The document "gemfire_validation_doc.md" can be used to validate Gemfire as 
+     well as provide an introduction to some of the GemFire cli (gfsh) commands. 
+     Run the following to Read the validation doc:    
+       cat /opt/pivotal/gemfire/gemfire_validation_doc.md | more
 
 Connect to Gemfire cli
    - As user gfadmin, from the Linux command prompt:  gfsh
@@ -35,9 +46,6 @@ Gemfire Documentation
 Learn more about Gemfire
    - Gemfire 30 second review: https://www.youtube.com/watch?v=AEpILFGX8y0
    - Introduction to Gemfire Product Features: https://www.youtube.com/watch?v=q52QlWkdFgE
-
-Validation steps
-   - See file: gemfire_validation.txt located: /opt/pivotal/gemfire
 *********************************************************************************
 _EOF
 
@@ -46,6 +54,3 @@ echo $cmd
 eval $cmd
 
 echo "### Leaving script $0 .........."
-
-
-
