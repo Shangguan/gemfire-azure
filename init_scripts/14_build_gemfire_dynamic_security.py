462938020d63cfd28660719c9a07c046699b226e
wrmay <rmay@pivotal.io>
2017-10-20 15:42:06 -0400

    run mvn package

--- init_scripts/14_build_gemfire_dynamic_security.py
+++ init_scripts/14_build_gemfire_dynamic_security.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     runAsUser = os.environ['GEMFIRE_USER']
     build_dir = '/home/{0}/gemfire-azure/gemfire-dynamic-security'.format(runAsUser)
 
-    p = subprocess.Popen([MAVEN], cwd = build_dir,  stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    p = subprocess.Popen([MAVEN, 'package'], cwd = build_dir,  stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     output = p.communicate()
     if p.returncode != 0:
-        raise Exception('"{0}" failed with the following output: {1}'.format(' '.join(list(args)), output[0]))
+        raise Exception('maven build failed with the following output: {0}'.format(output[0]))
 
     subprocess.check_output(['chown', '-R', runAsUser, build_dir])
     print 'built gemfire-dynamic-security'
