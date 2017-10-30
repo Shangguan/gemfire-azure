551351849c2412d647623e302eb7f0a93f1c00e4
wrmay <rmay@pivotal.io>
2017-10-30 14:08:57 -0400

    add cluster init step to set pdx-read-serialized true

---
+++ init_scripts/13_init_gemfire_cluster.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+import jinja2
+import json
+import os
+import os.path
+import random
+import shutil
+import pwd
+import sys
+import subprocess
+
+def validate_env():
+    for key in ['GEMFIRE_USER', 'CLUSTER_NAME']:
+        if key not in os.environ:
+            sys.exit('A required environment variable is not present: ' + key)
+
+if __name__ == '__main__':
+    """
+    This script places a cluster definition and the gemfire-manager cluster
+    control scripts in /home/$GEMFIRE_USER/gemfire_cluster
+
+    This script expects the following environment variables
+    GEMFIRE_USER the user that will run the GemFire processes
+    CLUSTER_NAME the name of the cluster
+    """
+    validate_env()
+
+    # parameters
+    clusterName = os.environ['CLUSTER_NAME']
+    gemuser = os.environ['GEMFIRE_USER']
+
+    # check to see if we are the 0th host
+    hostname = subprocess.check_output(['hostname'])
+    if hostname == clusterName + '-server0':
+        cluster_home = '/datadisks/disk1/gemfire_cluster'
+        java_home = '/usr/java/jdk1.8.0_144'
+        gemfire = '/usr/local/gemfire'
+
+        rc = subprocess.call(['sudo','-u',gemuser, 'GEMFIRE={0}'.format(gemfire),'JAVA_HOME={0}'.format(java_home), 'python', 'cluster.py','start'], cwd=cluster_home)
+        if (rc == 0):
+            print('First locator started')
+        else:
+            print('First locator failed to start')
+            sys.exit(-1)
+
+    else:
+        print('nothing to do for init_gemfire_cluster on this server')
+        sys.exit(0)
+
+    # now figure out the locator port and run a gfsh import cluster config
+    clusterdef_file = os.path.join(cluster_home,'cluster.json')
+    with open(clusterdef_file, 'r') as f:
+        cdef = json.load(f)
+
+    locator_port = cdef['locator-properties']['port']
+    gfsh = os.path.join(gemfire,'bin','gfsh')
+    zipfile_name = os.path.join('/home',gemuser,'gemfire-azure','init_scripts','cluster.zip')
+
+    subprocess.call([gfsh,'-e','connect --locator=localhost{0}'.format(locator_port), '-e','import cluster configuration --zip-file-name={0}'.format(zipfile_name)])
+    if (rc == 0):
+        print('initial cluster configuration imported')
+    else:
+        sys.exit('cluster configuration import failed')
+
+    # now signal readiness by starting a simple HttpServer
+    os.subprocess.Popen(['python', '-m', 'SimpleHttpServer', '80'], cwd=os.path.join('/home',gemuser,'gemfire-azure','init_scripts','http'))
--- init_scripts/16_start_gemfire.py
+++ init_scripts/16_start_gemfire.py
@@ -1,30 +1,59 @@
 from __future__ import print_function
+import httplib
 import os
 import subprocess
 import sys
+import time
 
 def validate_env():
-    for key in ['GEMFIRE_USER']:
+    for key in ['GEMFIRE_USER','CLUSTER_NAME','STARTING_PRIVATE_IP','PRIVATE_IP_PREFIX']:
         if key not in os.environ:
             sys.exit('A required environment variable is not present: ' + key)
 
+def waitforprimary():
+    for _ in range(100):
+        conn = httplib.HTTPConnection(primary_ip)
+        try:
+            conn.request('GET','/ready')
+            response = conn.getresponse()
+            if int(response.status) >= 200 and int(response.status) < 300 :
+                response.read()
+                break
+        finally:
+            conn.close()
+
+        print('waiting for primary locator')
+        time.sleep(3)
 
 if __name__ == '__main__':
     """
     This script expects the following environment variables
     GEMFIRE_USER
+    CLUSTER_NAME
+    STARTING_PRIVATE_IP
+    PRIVATE_IP_PREFIX
     """
     validate_env()
 
     gemuser = os.environ['GEMFIRE_USER']
+    clusterName = os.environ['CLUSTER_NAME']
+    start_ip = int(os.environ['STARTING_PRIVATE_IP'])
+    private_ip_prefix = os.environ['PRIVATE_IP_PREFIX']
+
     cluster_home = '/datadisks/disk1/gemfire_cluster'
     java_home = '/usr/java/jdk1.8.0_144'
     gemfire = '/usr/local/gemfire'
 
-    rc = subprocess.call(['sudo','-u',gemuser, 'GEMFIRE={0}'.format(gemfire),'JAVA_HOME={0}'.format(java_home), 'python', 'cluster.py','start'], cwd=cluster_home)
-    if (rc == 0):
-        print('GemFire cluster started')
-    else:
-        print('GemFire cluster failed to start')
+    primary_ip = '{0}{1}'.format(private_ip_prefix,start_ip)
 
-    sys.exit(rc)
+    if hostname != clusterName + '-server0':
+
+        waitforprimary()
+
+        rc = subprocess.call(['sudo','-u',gemuser, 'GEMFIRE={0}'.format(gemfire),'JAVA_HOME={0}'.format(java_home), 'python', 'cluster.py','start'], cwd=cluster_home)
+        if (rc == 0):
+            print('GemFire cluster started')
+        else:
+            print('GemFire cluster failed to start')
+
+        sys.exit(rc)
