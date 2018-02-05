d626e3ed249ad4ce94b7157caf311c8c8a32d26f
wrmay <rmay@pivotal.io>
2018-02-05 16:22:14 -0500

    update disk partitioning and formatting logic

--- init_scripts/08_prepare_data_disks.py
+++ init_scripts/08_prepare_data_disks.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     This script creates a file system on /dev/sdc, mounts it and
     creates an fstab entry.
 
-    !! It currently handles on one data disk. !!
+    !! It currently handles only one data disk. !!
+
+    !! It does not check for the presence of data before formatting !!
 
     It must be run as root.
 
@@ -17,14 +19,18 @@ if __name__ == '__main__':
     mount_point = '/datadisks/disk1'
     fstype = 'ext4'
 
-    subprocess.check_call(['mkfs','-t',fstype,device])
+    subprocess.check_output(['parted', device, 'mklabel', 'gpt', 'mkpart', 'primary', 'ext2', '0%', '100%'])
+    print('partitioned {0}'.format(device))
+
+    subprocess.check_call(['mkfs','-t',fstype, device + '1'])
     print('formatted {0}'.format(device))
 
     subprocess.check_call(['mkdir','-p',mount_point])
-    subprocess.check_call(['mount','-t',fstype,device,mount_point])
+    subprocess.check_call(['mount','-t',fstype,device + '1',mount_point])
+    print('mounted {0} on {1}'.format(device,mount_point))
 
     fstab_entry = '{0}  {1} {2} default 0 0'.format(device,mount_point,fstype)
     with open('/etc/fstab','a') as fstab_file:
         fstab_file.write('\n' + fstab_entry)
 
-    print('mounted {0} on {1}'.format(device,mount_point))
+    print('updated fstab for {0}'.format(device + '1'))
